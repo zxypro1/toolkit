6a919224c6de2904b46c1ea0922bcb420c765c2e
wss <wssgryx@163.com>
2023-06-16 16:50:05 +0800

    feat(utils): 新增获取 globalConfig
    Signed-off-by: wss <wssgryx@163.com>

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,5 +3,6 @@ export { default as isCiCdEnvironment } from './is-cicd-environment';
 export { default as isChinaUser } from './is-china-user';
 export { default as isDebugMode } from './is-debug-mode';
 export { default as getRootHome } from './get-root-home';
+export { default as getGlobalConfig, GLOBAL_CONFIG_FILE_PATH } from './get-global-config';
 export { default as parseArgv } from './parse-argv';
 export { default as getYamlContent, getYamlPath } from './get-yaml-content';
