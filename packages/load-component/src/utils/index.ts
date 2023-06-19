cad8bc89bbf86769157f2996a86f8f8fbabc64f9
xsahxl <xsahxl@126.com>
2023-06-19 18:31:09 +0800

    feat: ✨ load-component dev
    Signed-off-by: xsahxl <xsahxl@126.com>

--- packages/load-component/src/utils/index.ts
+++ packages/load-component/src/utils/index.ts
@@ -1,6 +1,11 @@
 import path from 'path';
 import fs from 'fs-extra';
-import { get } from 'lodash';
+import { get, includes, find, split, filter, set, isEmpty } from 'lodash';
+import axios from 'axios';
+import { getRootHome } from '@serverless-devs/utils';
+
+const debug = require('@serverless-cd/debug')('serverless-devs:load-component');
+
 
 export function readJsonFile(filePath: string) {
     if (fs.existsSync(filePath)) {
@@ -41,8 +46,58 @@ export const buildComponentInstance = async (componentPath: string, params?: any
         if (componentInstance) {
             componentInstance.__path = componentPath;
         }
+        debug('load component success')
         return componentInstance;
     } catch (error) {
         return ChildComponent;
     }
 }
+
+
+
+export function getProvider(name: string) {
+    const [provider, component] = includes(name, '/') ? split(name, '/') : [null, name];
+    const [componentName, componentVersion] = split(component, '@');
+    const { core_load_serverless_devs_component } = process.env;
+    if (core_load_serverless_devs_component) {
+        const componentList = filter(split(core_load_serverless_devs_component, ';'), (v) => includes(v, '@'));
+        const componentNames = [];
+        const obj: any = {};
+        for (const item of componentList) {
+            const [n, v] = split(item, '@');
+            componentNames.push(n);
+            obj[n] = v;
+        }
+        const key = provider ? `${provider}/${componentName}` : componentName;
+        if (find(componentNames, (v) => v === key)) {
+            return [provider, componentName, obj[key]];
+        }
+    }
+    return [provider, componentName, componentVersion];
+}
+
+export const getZipballUrl = async (provider: string, componentName: string, componentVersion: string) => {
+    const base = provider ? `http://registry.devsapp.cn/simple/${provider}/${componentName}/releases` : `http://registry.devsapp.cn/simple/${componentName}/releases`;
+    const url = componentVersion ? base : `${base}/latest`;
+    debug(`url: ${url}`);
+    const response = await axios.get(url);
+    const data = get(response, 'data.Response');
+    let zipball_url = get(data, 'zipball_url');
+    if (componentVersion) {
+        const obj = find(data, (v) => v.tag_name === componentVersion);
+        zipball_url = get(obj, 'zipball_url');
+    }
+    if (isEmpty(zipball_url)) throw new Error(`url: ${url} is not found`);
+    return zipball_url;
+}
+
+export const getComponentCachePath = (provider: string, componentName: string, componentVersion: string) => {
+    if (provider) {
+        return path.join(getRootHome(), 'components', 'devsapp.cn', provider, componentVersion ? `${componentName}@${componentVersion}` : componentName)
+
+    }
+    return path.join(getRootHome(), 'components', 'devsapp.cn', componentVersion ? `${componentName}@${componentVersion}` : componentName)
+
+};
+
+export const getLockFile = (basePath: string) => path.join(basePath, '.s.lock');
