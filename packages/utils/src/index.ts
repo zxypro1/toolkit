3f3ff69f2f81875c66fc328a8b465d395754dc76
wss <wssgryx@163.com>
2023-06-19 13:24:19 +0800

    feat(uitl): 新增 setGlobalConfig 方法
    Signed-off-by: wss <wssgryx@163.com>

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,7 +3,7 @@ export { default as isCiCdEnvironment } from './is-cicd-environment';
 export { default as isChinaUser } from './is-china-user';
 export { default as isDebugMode } from './is-debug-mode';
 export { default as getRootHome } from './get-root-home';
-export { default as getGlobalConfig, GLOBAL_CONFIG_FILE_PATH } from './get-global-config';
+export { setGlobalConfig, getGlobalConfig, GLOBAL_CONFIG_FILE_PATH } from './global-config';
 export { default as parseArgv } from './parse-argv';
 export { default as getYamlContent, getYamlPath, getAbsolutePath } from './get-yaml-content';
 export { default as fieldEncryption } from './field-encryption';
