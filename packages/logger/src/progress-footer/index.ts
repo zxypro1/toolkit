8d8a52b380d8e034a0af2e7f9151e57ed23d7d92
xsahxl <xsahxl@126.com>
2023-07-21 17:07:09 +0800

    feat: update logger use map
    Signed-off-by: xsahxl <xsahxl@126.com>

--- packages/logger/src/progress-footer/index.ts
+++ packages/logger/src/progress-footer/index.ts
@@ -1,51 +1,43 @@
-import { IPropsOptions, IMateValue, IShowList } from './types';
-import defaultOptions from './default-options';
+import { IPropsOptions, IMateValue } from './types';
+import { blackBright, greenBright } from 'chalk';
 const CliProgressFooter = require('cli-progress-footer');
 
-export default class ProgressFooter {
+export default class ProgressFooter extends Map<string, IMateValue> {
   private progress: typeof CliProgressFooter;
-  private showList: IShowList;
-  private format: (showList: IShowList) => string[];
-
-  constructor(options: IPropsOptions = {}) {
+  constructor(private options: IPropsOptions = {}) {
+    super();
     this.progress = CliProgressFooter();
-    this.showList = new Map();
-
-    const o = { ...defaultOptions, ...options };
-    this.format = o.format;
-
-    // 确保 this 指向
-    this.reader = this.reader.bind(this);
-    this.clear = this.clear.bind(this);
-    this.upsert = this.upsert.bind(this);
-    this.removeItem = this.removeItem.bind(this);
-  }
-
-  public getKeys() {
-    return this.showList.keys();
+    this.options.format = typeof options.format === 'function' ? options.format : this.defaultFormat;
   }
 
   /**
    * 渲染方法
    */
-  public reader() {
-    // 如果不存在输出时，清空定时器
-    if (this.showList.size === 0) {
+  public reader = () => {
+    if (this.size === 0) {
       this.progress.updateProgress();
       return;
     }
     // 存在输出时再处理显示
-    const show = this.format(this.showList);
+    const show = this.options.format?.(this.entries());
     this.progress.updateProgress(show);
   }
-
-
+  private defaultFormat = (data: IterableIterator<[string, IMateValue]>) => {
+    const show: string[] = [];
+    for (const [key, value] of data) {
+      const { message } = value;
+      const showMessage = `${blackBright(key)} \n ${greenBright(message)}`;
+      show.push(showMessage);
+    }
+    return show;
+  }
   /**
    * 清理输出：将定时器、事件监听、动态输出都清理掉
    */
   public clear() {
     this.progress.updateProgress();
-    this.showList.clear();
+    // 避免循环引用
+    super.clear();
   }
 
   /**
@@ -54,19 +46,18 @@ export default class ProgressFooter {
    * @param message
    */
   public upsert(id: string, message: string) {
-    if (this.showList.has(id)) {
-      const config = this.showList.get(id) as IMateValue;
-      this.showList.set(id, {
+    if (this.has(id)) {
+      const config = this.get(id) as IMateValue;
+      this.set(id, {
         ...config,
         message,
       });
     } else {
-      this.showList.set(id, {
+      this.set(id, {
         timer: new Date().getTime(),
         message,
       });
     }
-
     this.reader();
   }
 
@@ -75,6 +66,6 @@ export default class ProgressFooter {
    * @param id
    */
   public removeItem(id: string) {
-    this.showList.delete(id);
+    this.delete(id);
   }
 }
--- packages/logger/src/progress-footer/types.ts
+++ packages/logger/src/progress-footer/types.ts
@@ -1,16 +1,7 @@
 
 
-export type IShowList = Map<string, IMateValue>;
-
-export interface IFormatOptions {
-  /**
-   * 动态配置
-   */
-  frames: string[];
-}
-
 export interface IPropsOptions {
-  format?: (showList: IShowList) => string[];
+  format?: (data: IterableIterator<[string, IMateValue]>) => string[];
 }
 
 export interface IMateValue {
