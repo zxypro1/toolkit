88389f949e618165c45f7c970728a4747cc8c27c
xsahxl <xsahxl@126.com>
2023-07-28 17:53:11 +0800

    publish: utils and registry
    Signed-off-by: xsahxl <xsahxl@126.com>

--- packages/utils/src/global-config.ts
+++ packages/utils/src/global-config.ts
@@ -10,16 +10,16 @@ const rootHome = getRootHome();
 // 全局配置文件存放位置
 export const GLOBAL_CONFIG_FILE_PATH = path.join(rootHome, 'set-config.yml');
 
-export const getGlobalConfig = (key: string) => {
+export const getGlobalConfig = (key: string, fallback?: string) => {
   const content = getYamlContent(GLOBAL_CONFIG_FILE_PATH);
-  return content?.[key];
+  return content?.[key] || fallback;
 };
 
 export const setGlobalConfig = (key: string, value: unknown) => {
   // 创建 a 目录以及其子目录
   fs.mkdirSync(rootHome, { recursive: true });
 
-  const content = getYamlContent(GLOBAL_CONFIG_FILE_PATH);
+  const content = getYamlContent(GLOBAL_CONFIG_FILE_PATH) || {};
   content[key] = value;
 
   fs.writeFileSync(GLOBAL_CONFIG_FILE_PATH, yaml.dump(content));
