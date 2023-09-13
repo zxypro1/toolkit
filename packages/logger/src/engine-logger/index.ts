7988dca826eee6f65973a7780a27e07e0686498a
xsahxl <xsahxl@126.com>
2023-09-13 13:30:19 +0800

    feat: logger.output add return value
    Signed-off-by: xsahxl <xsahxl@126.com>

--- packages/logger/src/engine-logger/index.ts
+++ packages/logger/src/engine-logger/index.ts
@@ -60,6 +60,7 @@ export default class EngineLogger extends Logger {
   output(content: Record<string, any>, indent?: number, options?: RendererOptions) {
     const message = prettyjson.render(content, { keysColor: 'bold', emptyArrayMsg: '[]', ...options }, indent);
     this.write(message);
+    return message;
   }
   // TODO: 仅提示但不报错
   tips(message: string, tips?: string) {
