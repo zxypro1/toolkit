c9e894af560210a4d3cb03153cc84218c12b75cb
xsahxl <xsahxl@126.com>
2023-09-18 17:27:09 +0800

    fix: env.json => default-env.json
    Signed-off-by: xsahxl <xsahxl@126.com>

--- packages/parse-spec/src/contants/index.ts
+++ packages/parse-spec/src/contants/index.ts
@@ -5,4 +5,4 @@ export const REGX = /\${([\w\W]*?)}/;
 export const REGXG = /\${([\w\W]*?)}/g;
 export const ENVIRONMENT_KEY = 'env';
 export const ENVIRONMENT_FILE_NAME = 'env.yaml';
-export const ENVIRONMENT_FILE_PATH = path.join(utils.getRootHome(), 'cache', 'env.json');
+export const ENVIRONMENT_FILE_PATH = path.join(utils.getRootHome(), 'cache', 'default-env.json');
