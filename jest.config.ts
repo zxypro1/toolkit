1e1337da38a4f1e9335f40eeb8d477c95a81b5b9
neil.zxy <neil.zxy@alibaba-inc.com>
2024-01-09 10:05:58 +0800

    add test coverage settings

--- jest.config.ts
+++ jest.config.ts
@@ -7,5 +7,15 @@ export default {
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'packages/**/*.js', 
+    'packages/**/*.ts', 
+    '!**/node_modules/**',
+    '!**/__tests__/**',
+    '!**/lib/**',
+  ],
   verbose: true,
 };
